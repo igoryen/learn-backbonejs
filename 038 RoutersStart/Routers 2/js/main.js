94eb71ee453193a7e97bc46e8d535ef62f194572
igoryen <igor.yentaltsev@gmail.com>
2021-12-16 18:49:24 -0500

    039 added Views

--- 038 RoutersStart/Routers 2/js/main.js
+++ 038 RoutersStart/Routers 2/js/main.js
@@ -2,3 +2,24 @@
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+var ArtistView = Backbone.View.extend({
+    render() {
+        this.$el.html("ARTISTS VIEW");
+        return this;
+    }
+});
+
+var AlbumsView = Backbone.View.extend({
+    render() {
+        this.$el.html("ALBUMS VIEW");
+        return this;
+    }
+});
+
+var GenresView = Backbone.View.extend({
+    render() {
+        this.$el.html("GENRES VIEW");
+        return this;
+    }
+});
